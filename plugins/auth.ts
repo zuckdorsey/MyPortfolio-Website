1c70d6fe655b5438c191cea650b7fd8283950bcd
Ababil <ababilmustaqim@proton.me>
2025-11-06 01:16:43 +0700

    commit

--- plugins/auth.ts
+++
@@ -1,9 +0,0 @@
-// This plugin ensures that auth is initialized on both client and server
-import useAuth from '~/composables/auth/useAuth';
-
-export default defineNuxtPlugin(() => {
-  const auth = useAuth();
-  
-  // Initialize auth state
-  auth.initAuth();
-});
